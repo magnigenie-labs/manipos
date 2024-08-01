defd294fc5bfdeaa6ee6b68f0c712341ad2befae
Bikramaditya <bikram.magnigeeks@gmail.com>
2024-08-01 17:07:26 +0530

    - Fixed : bug fixes and improvements

--- dev/flutter_bootstrap.js
+++ dev/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2919639586"
+    serviceWorkerVersion: "997948072"
   }
 });
